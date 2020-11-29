7cfb72fa986bcedefb1f3c9f1a502ab876d9b741
chaoannricardo <chaoannricardo@protonmail.com>
2020-11-29 21:46:15 +0800

    [up] final version of ASS09

--- Assignments/R08546036_GeneticAlgorithmLibrarySolutions/GeneticAlgorithmLibrary/PermutationGASolver.cs
+++ Assignments/R08546036_GeneticAlgorithmLibrarySolutions/GeneticAlgorithmLibrary/PermutationGASolver.cs
@@ -279,13 +279,24 @@ namespace GeneticAlgorithmLibrary
                         if (!(chromosomes[child1Idx].Contains(chromosomes[motherIdx][i])))
                         {
                             if (temp_num == index_1) temp_num = index_2;
-                            chromosomes[child1Idx][temp_num] = chromosomes[motherIdx][i];
+                            try {
+                                chromosomes[child1Idx][temp_num] = chromosomes[motherIdx][i];
+                            }
+                            catch (System.IndexOutOfRangeException)
+                            {
+                            }
+
                             temp_num += 1;
                         }
                         if (!(chromosomes[child2Idx].Contains(chromosomes[fatherIdx][i])))
                         {
                             if (temp_num_2 == index_1) temp_num_2 = index_2;
-                            chromosomes[child2Idx][temp_num] = chromosomes[fatherIdx][i];
+                            try {
+                                chromosomes[child2Idx][temp_num] = chromosomes[fatherIdx][i];
+                            }
+                            catch (System.IndexOutOfRangeException) {                           
+                            }
+                            
                             temp_num_2 += 1;
                         }
                     }
@@ -346,7 +357,126 @@ namespace GeneticAlgorithmLibrary
                     }
 
                     break;
-                case CrossoverType.OCCC:
+                default:
+                    // i1, i2 cut locations 
+                    // m[] partial map, m[i] => the mapping target of i
+
+                    // define two crossover map for chromosome
+                    index_1 = randomizer.Next(numberOfGenes);
+                    while (index_2 == index_1)
+                    {
+                        index_2 = randomizer.Next(numberOfGenes);
+                    }
+                    // swap two numbers if index_1 is larger
+                    if (index_2 < index_1)
+                    {
+                        temp_num = index_1;
+                        index_1 = index_2;
+                        index_2 = temp_num;
+                    }
+                    // initiate mapping 
+                    mapping = new int[numberOfGenes];
+                    for (int i = 0; i < mapping.Length; i++) mapping[i] = -1;
+                    // build up the mapping
+                    for (int i = index_1; i < index_2; i++)
+                    {
+
+                        if (chromosomes[fatherIdx][i] == chromosomes[motherIdx][i]) continue;
+                        if (mapping[chromosomes[fatherIdx][i]] == -1 && mapping[chromosomes[motherIdx][i]] == -1)
+                        {
+                            mapping[chromosomes[fatherIdx][i]] = chromosomes[motherIdx][i];
+                            mapping[chromosomes[motherIdx][i]] = chromosomes[fatherIdx][i];
+                        }
+                        else if (mapping[chromosomes[fatherIdx][i]] == -1)
+                        {
+                            mapping[chromosomes[fatherIdx][i]] = mapping[chromosomes[motherIdx][i]];
+                            try
+                            {
+                                mapping[mapping[chromosomes[motherIdx][i]]] = chromosomes[fatherIdx][i];
+                            }
+                            catch (System.IndexOutOfRangeException Exception)
+                            {
+
+                            }
+
+                            mapping[chromosomes[motherIdx][i]] = -2;
+                        }
+                        else if (mapping[chromosomes[motherIdx][i]] == -1)
+                        {
+                            try
+                            {
+                                mapping[chromosomes[motherIdx][i]] = mapping[chromosomes[fatherIdx][i]];
+                            }
+                            catch (System.IndexOutOfRangeException Exception) { }
+                            try
+                            {
+                                mapping[mapping[chromosomes[fatherIdx][i]]] = chromosomes[motherIdx][i];
+                            }
+                            catch (System.IndexOutOfRangeException Exception) { }
+                            try
+                            {
+                                mapping[chromosomes[fatherIdx][i]] = -2;
+                            }
+                            catch (System.IndexOutOfRangeException Exception) { }
+                        }
+                        else
+                        {
+                            try
+                            {
+                                mapping[mapping[chromosomes[motherIdx][i]]] = mapping[chromosomes[fatherIdx][i]];
+                            }
+                            catch (System.IndexOutOfRangeException Exception) { }
+
+                            try
+                            {
+                                mapping[mapping[chromosomes[fatherIdx][i]]] = mapping[chromosomes[motherIdx][i]];
+                            }
+                            catch (System.IndexOutOfRangeException Exception) { }
+
+
+                            mapping[chromosomes[fatherIdx][i]] = -3;
+                            mapping[chromosomes[motherIdx][i]] = -3;
+                        }
+                    }
+
+                    // crossover and make two children
+                    for (int i = 0; i < numberOfGenes; i++)
+                    {
+                        if (index_1 <= i && i < index_2)
+                        {
+                            chromosomes[child1Idx][i] = chromosomes[motherIdx][i];
+                            chromosomes[child2Idx][i] = chromosomes[fatherIdx][i];
+                        }
+                        else
+                        {
+                            if (mapping[chromosomes[fatherIdx][i]] < 0) chromosomes[child1Idx][i] = chromosomes[fatherIdx][i];
+                            else chromosomes[child1Idx][i] = mapping[chromosomes[fatherIdx][i]];
+
+                            if (mapping[chromosomes[motherIdx][i]] < 0) chromosomes[child2Idx][i] = chromosomes[motherIdx][i];
+                            else chromosomes[child2Idx][i] = mapping[chromosomes[motherIdx][i]];
+                        }
+                    }
+                    // crossover finished.
+
+                    // check if all gene are distinct, if not let other chromosome replace it 
+                    for (int i = 0; i < populationSize * 3; i++)
+                    {
+                        temp = new int[numberOfGenes];
+                        for (int j = 0; j < numberOfGenes; j++)
+                        {
+                            temp[j] = j;
+                        }
+
+                        if (chromosomes[i].Distinct().ToArray().Count() != numberOfGenes)
+                        {
+                            chromosomes[i] = temp.OrderBy(x => randomizer.Next()).ToArray();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
                     break;
             }
 
@@ -397,6 +527,18 @@ namespace GeneticAlgorithmLibrary
                 for (int j = 0; j < numberOfGenes; j++)
                 {
                     objValue += chromosomes[i][j] * MachineJobWeight[j];
+
+                    // penalty:
+                    if (chromosomes[i].Distinct().ToArray().Count() < numberOfGenes) {
+                        switch (optimizationType) {
+                            case OptimizationType.Maximization:
+                                objValue -= 100 * numberOfGenes;
+                                break;
+                            case OptimizationType.Minimization:
+                                objValue += 100 * numberOfGenes;
+                                break;
+                        }                    
+                    }
                 }
                 objectiveValues[i] = objValue;
             }
